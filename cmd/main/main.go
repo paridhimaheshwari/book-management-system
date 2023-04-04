830f34b86966514e79e0e30b82c3e389ff256307
paridhimaheshwari <priti.paridhi@gmail.com>
2023-04-04 18:04:56 +0530

    Fixed routes

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,13 +5,28 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/paridhimaheshwari/book-management-system/pkg/config"
 	"github.com/paridhimaheshwari/book-management-system/pkg/routes"
 )
+var db *gorm.DB
 
 func main() {
 	r := mux.NewRouter()
-	routes.RegisterBookStoreRoute(r)
+	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 
 }
+
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book(
+	db.NewRecord(b)
+	db.Create(&b)
+)
--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,13 +1,11 @@
 package routes
 
 import (
-	"webserve/pkg/controllers"
-
 	"github.com/gorilla/mux"
 	"github.com/paridhimaheshwari/book-management-system/pkg/controllers"
 )
 
-var RegisterBookStoreRoute = func(router *mux.Router) {
+var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
